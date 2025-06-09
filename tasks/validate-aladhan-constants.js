1748ee95b89467c2b34ea5db29fbccaa36996bb1
MahdiRazzaque <mahdi.razzaque+github@gmail.com>
2025-06-09 17:52:28 +0100

    Remove unused files

--- tasks/validate-aladhan-constants.js
+++
@@ -1,8 +0,0 @@
-// tasks/validate-aladhan-constants.js
-
-import { validateConstants } from '../src/prayer/validate-constants.js';
-
-console.log('🧪 Running Aladhan constants validation...');
-validateConstants().then(() => {
-    console.log('✅ Validation script completed');
-}); 
